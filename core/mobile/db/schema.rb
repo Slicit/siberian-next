ab0ba308408c33ac9dbf8fef6a19018b6b52884d
Alex-Yves CRÉPIN <contact@net-reflect.com>
2026-08-29 12:29:27 -0400

    chore: regenerate the mobile schema for follow_device_scheme

--- core/mobile/db/schema.rb
+++ core/mobile/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_29_120000) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_29_210000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_29_120000) do
     t.string "bundle_identifier", null: false
     t.datetime "created_at", null: false
     t.string "domain", null: false
+    t.boolean "follow_device_scheme", default: true, null: false
     t.string "icon_path"
     t.string "name", null: false
     t.string "primary_color"
